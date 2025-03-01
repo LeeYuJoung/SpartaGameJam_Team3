5beacef5b3308ecf78a0963298339e968eb72b5a
LeeYuJoung <nania000822@gmail.com>
2025-03-01 23:41:24 +0900

    Update AntsMove.cs

--- Assets/02.Scripts/kmu/AntsMove.cs
+++ Assets/02.Scripts/kmu/AntsMove.cs
@@ -1,22 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< HEAD
-=======
+
 using UnityEditor.Rendering;
->>>>>>> main
 using UnityEngine;
 
 namespace KMU
 {
-<<<<<<< HEAD
-    public class AntsMove : MonoBehaviour
-    {
-=======
     public enum AntColor { Red = 3, Blue = 4, Yellow = 5 }
     public class AntsMove : MonoBehaviour
     {
         private float speed = 2f;
->>>>>>> main
         // Start is called before the first frame update
         void Start()
         {
@@ -30,8 +23,3 @@ namespace KMU
         }
     }
 }
-
-<<<<<<< HEAD
-=======
-
->>>>>>> main
