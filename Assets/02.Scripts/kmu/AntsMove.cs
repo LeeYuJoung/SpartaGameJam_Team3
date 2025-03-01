99462b5e7d72be3add3cb8fbf7876d88a8625696
minuYoi <kmwoo403@naver.com>
2025-03-01 15:51:34 +0900

    개미 이동 스크립트
    개미 이동 스크립트 생성

---
+++ Assets/02.Scripts/kmu/AntsMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+namespace KMU
+{
+    public enum AntColor { Red = 3, Blue = 4, Yellow = 5 }
+    public class AntsMove : MonoBehaviour
+    {
+        private float speed = 2f;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+
